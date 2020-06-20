98338587a953894c3c3d1ff2a7a5c8a698995e2f
Izz Garvi <igarvilo@gmail.com>
2020-06-20 12:19:06 +0100

    updated tests

--- tests/test.py
+++ tests/test.py
@@ -1,27 +1,37 @@
+from logging import log
+
 from src.game_logic import GameLogic
 
-# choices = ["rock", "paper", "scissors", "lizard", "spock"]
+winning_choices = [{"rock": ["scissors", "lizard"]},
+               {"paper": ["rock", "spock"]},
+               {"scissors": ["paper", "lizard"]},
+               {"lizard": ["paper", "spock"]},
+               {"spock": ["rock", "scissors"]}]
+losing_choices = [{"rock": ["paper", "spock"]},
+               {"paper": ["scissors", "lizard"]},
+               {"scissors": ["rock", "spock"]},
+               {"lizard": ["rock", "scissors"]},
+               {"spock": ["paper", "lizard"]}]
 
 
 def test_no_inquirer_lose():
     game_logic = GameLogic()
-    user = "rock"
-    machine = "paper"
-
-    assert game_logic.winner(user, machine) == "YOU LOSE, SORRY"
+    for d in losing_choices:
+        for key, values in d.items():
+            for value in values:
+                assert game_logic.winner(key, value) == "YOU LOSE, SORRY"
 
 
 def test_no_inquirer_win():
     game_logic = GameLogic()
-    user = "paper"
-    machine = "rock"
-
-    assert game_logic.winner(user, machine) == "YOU WIN, YAY"
+    for d in winning_choices:
+        for key, values in d.items():
+            for value in values:
+                assert game_logic.winner(key, value) == "YOU WIN, YAY"
 
 
 def test_no_inquirer_tie():
     game_logic = GameLogic()
-    user = "scissors"
-    machine = "scissors"
-
-    assert game_logic.winner(user, machine) == "YOU TIED :|"
+    for d in winning_choices:
+        for key in d.keys():
+            assert game_logic.winner(key, key) == "YOU TIED :|"
